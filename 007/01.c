0f67287faab0722abd7dfcc7052d72de0cd5fee1
Mary Ann <tomasswaier@gmail.com>
2024-04-12 06:59:29 +0200

    dijkstra

--- 007/01.c
+++ 007/01.c
@@ -1,143 +1,266 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **array;
-int *writing;
-void debug(int N) {
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      printf("%d ", array[i][j]);
-    }
-    printf("\n");
-  }
-}
-void delete (int a, int b) {
-  if (array[a][b] != -1)
-    array[a][b] = -1;
+#define MAX_NODES 100
+#define INFINITY 1000000
+
+typedef struct Node {
+    int position;
+    int value;
+    struct Node *next;
+} Node;
+
+typedef struct Graph {
+    int size;
+    Node **nodes;
+    int adjacency[MAX_NODES][MAX_NODES];
+} Graph;
+
+int isFirst = 0;
+void printSpace() {
+  if (isFirst == 0)
+    isFirst++;
   else
-    printf("delete failed");
+    printf("\n");
 }
-void insert(int a, int b, int val) {
-  if (array[a][b] == -1) {
-    array[a][b] = val;
-  } else {
-    printf("inser faied");
-  }
+
+
+
+Graph *createGraph(int size) {
+    Graph *graph = (Graph *)malloc(sizeof(Graph));
+    graph->size = size;
+    graph->nodes = (Node **)calloc(size, sizeof(Node *));
+    for (int i = 0; i < size; i++) {
+        graph->nodes[i] = NULL;
+        for (int j = 0; j < size; j++) {
+            graph->adjacency[i][j] = 0;
+        }
+    }
+    return graph;
 }
-void update(int a, int b,int val){
 
-  if (array[a][b] == -1) {
-    array[a][b] = val;
-  } else {
-    printf("udate faied");
-  }
+void addEdge(Graph *graph, int a, int b, int val) {
+    if (graph->adjacency[a][b] != 0) {
+        printSpace(); 
+        printf("insert %d %d failed", a, b);
+        return;
+    }
+
+    Node *current = graph->nodes[a];
+    while (current != NULL) {
+        if (current->position == b) {
+            printSpace();
+            printf("Edge between %d and %d already exists.", a, b);
+            return;
+        }
+        current = current->next;
+    }
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->position = b;
+    newNode->value = val;
+    newNode->next = graph->nodes[a];
+    graph->nodes[a] = newNode;
+
+    graph->adjacency[a][b] = val;
+    graph->adjacency[b][a] = val;
 }
-void search(int start,int end){
-  int boolean=0;
-  
-  while(boolean!=1){
-    
 
-
-
-
-  }
-
-
-
-
+void printGraph(Graph *graph) {
+    printf("debugovanie:\n");
+    for (int i = 0; i < graph->size; i++) {
+        Node *current = graph->nodes[i];
+        printf("printujem %d:", i);
+        while (current != NULL) {
+            printf(" (%d, %d)", current->position, current->value);
+            current = current->next;
+        }
+        printf("\n");
+    }
 }
-void setWriting(int N){ //set all values in final tab to -1 = infinity
 
-  for(int i=0;i<N;i++)
-    writing[i]=-1;
-}
-void setup(int N){ // setup the array ( looked ugly in main);
+void deleteEdge(Graph *graph, int a, int b) {
+    if (graph->adjacency[a][b] == 0) {
+        printSpace();
+        printf("delete %d %d failed", a, b);
+        return;
+    }
 
-  array = (int **)calloc(N, sizeof(int *));
-  for (int i = 0; i < N; i++) {
-    array[i] = (int *)calloc(N, sizeof(int));
-  }
-  writing=malloc(N*sizeof(int));
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      array[i][j] = -1;
+    Node *current = graph->nodes[a];
+    Node *prev = NULL;
+
+    while (current != NULL) {
+        if (current->position == b) {
+            if (prev == NULL) {
+                graph->nodes[a] = current->next;
+            } else {
+                prev->next = current->next;
+            }
+            free(current);
+            break;
+        }
+        prev = current;
+        current = current->next;
+    }
+
+    graph->adjacency[a][b] = 0;
+    graph->adjacency[b][a] = 0;
 }
+
+
+void updateEdge(Graph *graph, int a, int b, int newVal) {
+    if (graph->adjacency[a][b] + newVal < 0) {
+        printSpace();
+        printf("update %d %d failed", b, a);
+        return;
+    }
+
+    Node *current = graph->nodes[a];
+    while (current != NULL) {
+        if (current->position == b) {
+            current->value = newVal;
+            break;
+        }
+        current = current->next;
+    }
+
+    graph->adjacency[a][b] += newVal;
+    graph->adjacency[b][a] += newVal;
+}
+
+void dijkstra(Graph *graph, int startNode, int endNode) {
+    int distances[MAX_NODES];
+    int prev[MAX_NODES];
+    int visited[MAX_NODES] = {0};
+
+
+    for (int i = 0; i < graph->size; i++) {
+        distances[i] = INFINITY;
+        prev[i] = -1;
+    }
+    distances[startNode] = 0;
+
+    for (int count = 0; count < graph->size - 1; count++) {
+        int minDistance = INFINITY;
+        int minNode = -1;
+
+
+        for (int i = 0; i < graph->size; i++) {
+            if (!visited[i] && distances[i] < minDistance) {
+                minDistance = distances[i];
+                minNode = i;
+            }
+        }
+
+        if (minNode == -1) {
+
+            break;
+        }
+
+        visited[minNode] = 1;
+
+
+        for (int i = 0; i < graph->size; i++) {
+            if (!visited[i] && graph->adjacency[minNode][i] &&
+                distances[minNode] != INFINITY &&
+                distances[minNode] + graph->adjacency[minNode][i] < distances[i]) {
+                distances[i] = distances[minNode] + graph->adjacency[minNode][i];
+                prev[i] = minNode;
+            }
+        }
+    }
+
+
+    int current = endNode;
+    int path[MAX_NODES];
+    int pathLength = 0;
+    while (current != startNode && prev[current] != -1) {
+        path[pathLength++] = current;
+        current = prev[current];
+    }
+    path[pathLength++] = startNode;
+
+    printSpace();
+    printf("%d: [", distances[endNode]);
+    for (int i = pathLength - 1; i >= 0; i--) {
+        printf("%d", path[i]);
+        if (i > 0) printf(", ");
+    }
+    printf("]");
+}
+
+void freeGraph(Graph *graph) {
+    for (int i = 0; i < graph->size; i++) {
+        Node *current = graph->nodes[i];
+        while (current != NULL) {
+            Node *temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(graph->nodes);
+    free(graph);
+}
+
 int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    getchar();
 
-  int N, M;
-  scanf("%d %d", &N, &M);
-  getchar();
-  setup(N);
-  for (int i = 0; i < M; i++) {
-    int temp1, temp2, val;
-    scanf("(%d, %d, %d)\n", &temp1, &temp2, &val);
+    Graph *graph = createGraph(N);
 
-    array[temp1 < temp2 ? temp1 : temp2][temp2 > temp1 ? temp2 : temp1] = val;
-  }
-
-  char mode = 'k';
-  while ((mode = getchar()) != EOF) {
-    switch (mode) {
-    case 's': {
-      setWriting(N);
-      int temp1, temp2;
-      scanf("%d %d", &temp1, &temp2);
-      if (temp1 < N && temp2 < N)
-        search(temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1);
-
-      else
-        printf("out of bounds\n");
-
-      break;
+    for (int i = 0; i < M; i++) {
+        int temp1, temp2, val;
+        scanf("(%d, %d, %d)\n", &temp1, &temp2, &val);
+        addEdge(graph, temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1, val);
     }
-    case 'g':
-      debug(N);
-      break;
-    case 'i': {
 
-      int temp1, temp2, val;
-      scanf("%d %d %d", &temp1, &temp2, &val);
-      if (temp1 < N && temp2 < N)
-        insert(temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1,
-               val);
-      else
-        printf("out of bounds");
-      break;
+    char mode = 'k';
+    while ((mode = getchar()) != EOF) {
+        switch (mode) {
+            case 's': {
+                int startNode, endNode;
+                scanf("%d %d", &startNode, &endNode);
+                dijkstra(graph, startNode, endNode);
+                break;
+            }
+            case 'g': {
+                printGraph(graph);
+                break;
+            }
+            case 'i': {
+                int temp1, temp2, val;
+                scanf("%d %d %d", &temp1, &temp2, &val);
+                if (temp1 < N && temp2 < N)
+                    addEdge(graph, temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1, val);
+                else
+                    printf("zly input");
+                break;
+            }
+            case 'd': {
+                int temp1, temp2;
+                scanf("%d %d", &temp1, &temp2);
+                if (temp1 < N && temp2 < N)
+                    deleteEdge(graph, temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1);
+                else
+                    printf("input nieje v hranicia\n");
+                break;
+            }
+            case 'u': {
+                int temp1, temp2, newVal;
+                scanf("%d %d %d", &temp1, &temp2, &newVal);
+                if (temp1 < N && temp2 < N)
+                    updateEdge(graph, temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1, newVal);
+                else
+                    printf("zle input\n");
+                break;
+            }
+            case '\n':
+                break;
+        }
     }
-    case 'd': {
 
-      int temp1, temp2;
-      scanf("%d %d", &temp1, &temp2);
-      if (temp1 < N && temp2 < N)
-        delete(temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1);
-      else
-        printf("out of bounds\n");
-
-      break;
-    }
-    case 'u': {
-
-      int temp1, temp2, val;
-      scanf("%d %d %d", &temp1, &temp2, &val);
-      if (temp1 < N && temp2 < N)
-        update(temp1 < temp2 ? temp1 : temp2, temp2 > temp1 ? temp2 : temp1,
-               val);
-      else
-        printf("out of bounds\n");
-
-      break;
-    }
-    case '\n':
-      break;
-    }
-  }
-
-  for (int i = 0; i < N; i++) {
-    free(array[i]);
-  }
-  free(array);
-  free(writing);
-
-  return 0;
+    freeGraph(graph);
+    return 0;
 }
+
